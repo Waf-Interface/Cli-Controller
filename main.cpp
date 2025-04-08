18e81178fe83b13c66c433af1465ade1b93966a8
mortza mansory <mortzamansory12@gmail.com>
2025-04-08 16:34:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,51 +7,36 @@
 #define CYAN "\033[36m"
 #define YELLOW "\033[33m"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     WafGhc waf;
 
-    if (argc < 2)
-    {
-        std::cout << RED << "Error: No command provided.\n"
-                  << RESET;
+    if (argc < 2) {
+        std::cout << RED << "Error: No command provided.\n" << RESET;
         waf.printManual();
         return 1;
     }
 
     std::string command = argv[1];
-    if (command == "--version")
-    {
+    if (command == "--version") {
         waf.showVersion();
     }
-    else if (command == "--pass")
-    {
-        waf.changePassword();
+    else if (command == "--user-add") {
+        waf.addUser();
     }
-    else if (command == "--check")
-    {
+    else if (command == "--user-remove") {
+        waf.removeUser();
+    }
+    else if (command == "--user-pass") {
+        waf.changeUserPassword();
+    }
+    else if (command == "--check") {
         waf.checkStatus();
     }
-    else if (command == "--info")
-    {
-        //  Todo: Adding info of our controller
-        //  To implant this:
-        /*
-        1. Update the backend for accepting database or text files for saving the user and pass data on it.(The backend should use it and keep updated it)
-        2. Then add a method for change password.
-        */
-       //  The change password should have a logistic interface that user can choice the username he wants to change the password of it.
-       //  Then its show first all the usernames we got then the user choice one then after it user change them and this controller save the data's on.
-       //  Its should also restart the service and other things to make changes.
+    else if (command == "--uninstall") {
+        waf.uninstall();
     }
-    else if (command == "--unistall")
-    {
-        waf.unistall();
-    }
-    else
-    {
-        std::cout << RED << "Error: Unknown command \"" << command << "\".\n"
-                  << RESET;
+    else {
+        std::cout << RED << "Error: Unknown command \"" << command << "\".\n" << RESET;
         waf.printManual();
     }
 
